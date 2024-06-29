5070f1cc45e9c4411c27a44a6222f89a1b159952
Krittapas Wongchai <1am8anana@gmail.com>
2024-06-29 23:51:19 +0700

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(children: [
+              Text("something"),
+              Text("something"),
+              Row(
+                children: [Icon(Icons.reviews), Text("170 Reviews")],
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: [Icon(Icons.dock), Text("PREP: 25 hr")],
+                  ),
+                  Column(
+                    children: [Icon(Icons.dock), Text("PREP: 25 hr")],
+                  ),
+                  Column(
+                    children: [Icon(Icons.dock), Text("PREP: 25 hr")],
+                  )
+                ],
+              )
+            ]),
+            Image.asset("images/hmmm.jpg")
+          ],
+        ),
+      ),
+    );
+  }
+}
